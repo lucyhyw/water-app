fac0e4f96e83120511652e4f1094b02287eb2543
Lucy <lucyhyw@gmail.com>
2016-02-06 16:48:16 -0500

    preliminary geolocation features

--- water/water.js
+++ water/water.js
@@ -1,7 +1,43 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Template.body.helpers({
+    map: function() {
+      function success(position) {
+      var mapcanvas = document.createElement('div');
+      mapcanvas.id = 'mapcontainer';
+      mapcanvas.style.height = '400px';
+      mapcanvas.style.width = '600px';
 
+      document.querySelector('article').appendChild(mapcanvas);
+
+      var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var options = {
+        zoom: 15,
+        center: coords,
+        mapTypeControl: false,
+        navigationControlOptions: {
+        	style: google.maps.NavigationControlStyle.SMALL
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+
+      var marker = new google.maps.Marker({
+          position: coords,
+          map: map,
+          title:"You are here!"
+      });
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      error('Geo Location is not supported');
+    }
+    }
+  })
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
@@ -16,8 +52,7 @@ if (Meteor.isClient) {
   });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
 }
